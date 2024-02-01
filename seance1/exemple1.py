736086e2e8d3924ffa76880aa4739882dce5808a
LassouedMohamed <lassouedmohamedd@gmail.com>
2024-02-01 14:42:15 +0100

    second commit

---
+++ seance1/exemple1.py
@@ -0,0 +1,8 @@
+
+nom="Ali" # nom est une variable de type str
+age=30 # age est une variable de type int
+salare = 1200.7654 # salaire est une variable de type float
+est_celibataire = True # est_celibataire est une variable de type booléen
+print(nom)
+#on affiche les informations par la methode print
+print("nom: ", nom, " age: ", age, " salaire: ", salare, " est_celibataire: ", est_celibataire,sep='')
